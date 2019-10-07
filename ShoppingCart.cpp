dd06fb9e90f6cbd177c796f93f7140c8c038ee65
Nuphlab <56048443+Nuphlab@users.noreply.github.com>
2019-10-06 20:39:44 -0600

    Update and rename ItemToPurchase.cpp to ShoppingCart.cpp

---
+++ ShoppingCart.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "ShoppingCart.h"
+using namespace std;
+
+ShoppingCart::ShoppingCart(string custName, string date) {
+   custName = "none";
+   date = "January 1, 2016";
+}
+void ShoppingCart::AddToCart() {
+
+}
+void ShoppingCart::RemoveFromCart() {
+
+}
+void ShoppingCart::UpdateQuantity() {
+
+}
+string ShoppingCart::GetName() {
+   return custName;
+}
+string ShoppingCart::GetDate() {
+
+}
+int ShoppingCart::GetQuantity() {
+
+}
+double ShoppingCart::GetTotalCost() {
+
+}
+void ShoppingCart::PrintCostAndQuantity() {
+
+}
+void ShoppingCart::PrintDescription() {
+
+}
